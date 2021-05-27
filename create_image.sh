c999a7b81d8def9967cd85f72ee6ae34488f0377
shao.jiacheng <shao.jiacheng@99cloud.net>
2021-05-27 23:44:53 +0800

    增加移动dist步骤

--- create_image.sh
+++ create_image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cp -r ../jianmu-ui/dist .
+
 docker login -u $JIANMU_DOCKER_USERNAME -p $JIANMU_DOCKER_PASSWORD
 
 docker build -f Dockerfile -t ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} .
