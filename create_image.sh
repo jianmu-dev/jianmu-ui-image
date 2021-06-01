4312b4b74b2cb4666498852a25fe8f685204c0fa
邵嘉诚 <603681955@qq.com>
2021-06-01 15:23:56 +0800

    增加对tag的判断

--- create_image.sh
+++ create_image.sh
@@ -6,11 +6,11 @@ docker login -u $JIANMU_DOCKER_USERNAME -p $JIANMU_DOCKER_PASSWORD
 
 docker build -f Dockerfile -t ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} .
 
+if [ "$JIANMU_IMAGE_TAG" != "latest" ];then
 echo "push tag ${JIANMU_IMAGE_TAG}"
-
 docker push ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:{$JIANMU_IMAGE_TAG}
-
 docker tag ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:latest
+fi
 
 echo "push tag latest"
 
