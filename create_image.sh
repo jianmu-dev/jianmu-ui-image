3dcb2b91f870348488eab257a051362b4b7ccfbd
邵嘉诚 <shao.jiacheng@99cloud.net>
2021-06-27 01:49:44 +0000

    update create_image.sh.

--- create_image.sh
+++ create_image.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+set -e
 
-cp -r ../jianmu-ui/dist .
+${JIANMU_CMD_PRE}
 
-docker login -u $JIANMU_DOCKER_USERNAME -p $JIANMU_DOCKER_PASSWORD
+docker login -u ${JIANMU_DOCKER_USERNAME} -p ${JIANMU_DOCKER_PASSWORD}
 
-docker build -f Dockerfile -t ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} .
+set -x
+
+docker build -f Dockerfile -t ${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} ${JIANMU_DOCKER_BUILD_PATH}
 
 if [ "$JIANMU_IMAGE_TAG" != "latest" ];then
-echo "push tag ${JIANMU_IMAGE_TAG}"
-docker push ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG}
+
+docker push ${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG}
 docker tag ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:${JIANMU_IMAGE_TAG} ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:latest
 fi
 
-echo "push tag latest"
 
-docker push ${JIANMU_DOCKER_USERNAME}/${JIANMU_IMAGE_NAME}:latest
+docker push ${JIANMU_IMAGE_NAME}:latest
